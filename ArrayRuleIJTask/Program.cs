9fa6370f76cfe16cf05b6f52de241652dfddffe0
Aleksey_Filkin <acid_wind@mail.ru>
2023-02-17 00:12:10 +0700

    ArrayRuleIJTask solved

--- ArrayRuleIJTask/Program.cs
+++ ArrayRuleIJTask/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿//Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Bведите количество строк: ");
+int i = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Bведите количество столбцов: ");
+int j = int.Parse(Console.ReadLine() ?? "");
+int[,] array = new int[i, j];
+FillArray(array);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
